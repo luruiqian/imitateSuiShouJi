562279c86ab2be6ceccaa8c1c4ffed5c90766077
15904011626@163.com <15904011626@163.com>
2018-03-07 17:28:49 +0800

    九悦驾校

--- Cashbook/app/src/main/java/com/cashbook/cashbook/MainActivity.java
+++ Cashbook/app/src/main/java/com/cashbook/cashbook/MainActivity.java
@@ -1,13 +1,28 @@
 package com.cashbook.cashbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = (Button) findViewById(R.id.btn);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
