b4f3aa8d701601e543c9a538dc480991acf9e6f2
luruiqian <luruiqian@gomeplus.com>
2018-08-21 18:57:59 +0800

    根据是否首次打开进行跳转

--- Cashbook/app/src/main/java/com/cashbook/cashbook/activity/LauncherActivity.kt
+++ Cashbook/app/src/main/java/com/cashbook/cashbook/activity/LauncherActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Message
 import android.support.v7.app.AppCompatActivity
+import com.cashbook.cashbook.MainActivity
 import com.cashbook.cashbook.R
 
 class LauncherActivity : AppCompatActivity() {
@@ -21,37 +22,54 @@ class LauncherActivity : AppCompatActivity() {
         jumpGuide()
     }
 
+    /**
+     * 根据是否首次运行程序判断跳转引导页还是首页
+     */
     private fun jumpGuide() {
+        mHandler = object : Handler() {
+            override fun handleMessage(msg: Message?) {
+                super.handleMessage(msg)
+                if (msg?.what == LAUNCH_DELAY) {
+                    if (isFirstRun()) {
+                        var intent = Intent()
+                        intent.setClass(this@LauncherActivity, GuideActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        var intent = Intent()
+                        intent.setClass(this@LauncherActivity, MainActivity::class.java)
+                        startActivity(intent)
+                    }
+                    //跳转后需要关闭页面
+                    finish()
+                }
+            }
+        }
         mThread = object : Thread() {
             override fun run() {
                 super.run()
                 mHandler.sendEmptyMessageDelayed(LAUNCH_DELAY, 2000)
             }
         }
-
-        mHandler = object : Handler() {
-            override fun handleMessage(msg: Message?) {
-                super.handleMessage(msg)
-                if (msg?.what == LAUNCH_DELAY) {
-                    var intent = Intent()
-                    intent.setClass(this@LauncherActivity, GuideActivity::class.java)
-                    startActivity(intent)
-                }
-            }
-        }
+        mThread.start()
     }
 
-    override fun onResume() {
-        super.onResume()
-        if (mThread.isAlive) {
-            mThread.start()
-        }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        if (mThread.isAlive) {
-            mThread.interrupt()
+    /**
+     * 判断是否是第一次启动程序 利用 SharedPreferences 将数据保存在本地
+     */
+    private fun isFirstRun(): Boolean {
+        //实例化SharedPreferences
+        var sharePreference = this.getSharedPreferences("share", android.content.Context.MODE_PRIVATE)
+        //实例化SharedPreferences 的editor
+        var editor = sharePreference.edit()
+        var isFirstRun = sharePreference.getBoolean("isFirstRun", true)
+        if (!isFirstRun) {
+            return false
+        } else {
+            //存储数据
+            editor.putBoolean("isFirstRun", false)
+            //提交数据
+            editor.commit()
+            return true
         }
     }
 
