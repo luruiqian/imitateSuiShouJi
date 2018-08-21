e501a0ff1662413e90c332ef4eaf9d5e14f73611
luruiqian <luruiqian@gomeplus.com>
2018-08-21 14:11:27 +0800

    添加kotlin插件，完善launcher页

--- Cashbook/app/src/main/java/com/cashbook/cashbook/activity/LauncherActivity.kt
+++ Cashbook/app/src/main/java/com/cashbook/cashbook/activity/LauncherActivity.kt
@@ -1,40 +1,59 @@
 package com.cashbook.cashbook.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import android.support.v7.app.AppCompatActivity
 import com.cashbook.cashbook.R
-class LauncherActivity : AppCompatActivity() {
-    private var isFirstLaunch: Boolean = true
 
-    private var mThread = LaunchThread()
+class LauncherActivity : AppCompatActivity() {
+
+    private lateinit var mHandler: Handler
+    private lateinit var mThread: Thread
+
+    private val LAUNCH_DELAY = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_launcher)
-        //mThread.start()
-        if (isFirstLaunch) {
-          //  Intent intent = new Intent(this,)
-//            intent.setClass(this,TestActivity::class.java)
 
-            startActivity(intent)
+        jumpGuide()
+    }
+
+    private fun jumpGuide() {
+        mThread = object : Thread() {
+            override fun run() {
+                super.run()
+                mHandler.sendEmptyMessageDelayed(LAUNCH_DELAY, 2000)
+            }
+        }
+
+        mHandler = object : Handler() {
+            override fun handleMessage(msg: Message?) {
+                super.handleMessage(msg)
+                if (msg?.what == LAUNCH_DELAY) {
+                    var intent = Intent()
+                    intent.setClass(this@LauncherActivity, GuideActivity::class.java)
+                    startActivity(intent)
+                }
+            }
         }
     }
 
     override fun onResume() {
         super.onResume()
-
-    }
-
-    class LaunchThread : Thread() {
-
-        override fun run() {
-            super.run()
-            Thread.sleep(2000)
-
+        if (mThread.isAlive) {
+            mThread.start()
         }
     }
 
+    override fun onStop() {
+        super.onStop()
+        if (mThread.isAlive) {
+            mThread.interrupt()
+        }
+    }
 
 }
 
---
+++ Cashbook/app/src/main/java/com/cashbook/cashbook/constants/Constant.kt
@@ -0,0 +1,8 @@
+package com.cashbook.cashbook.constants
+
+/**
+ * Created by luruiqian on 2018/8/20.
+ */
+class Constant {
+    public val LAUNCH_DELAY = "launch_delay"
+}
