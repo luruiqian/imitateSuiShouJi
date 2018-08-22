45212b7b524ed5f8e259e883c42d17c41321f893
luruiqian <luruiqian@gomeplus.com>
2018-08-22 18:46:14 +0800

    launcher页添加计时

--- Cashbook/app/src/main/java/com/cashbook/cashbook/activity/LauncherActivity.kt
+++ Cashbook/app/src/main/java/com/cashbook/cashbook/activity/LauncherActivity.kt
@@ -1,25 +1,50 @@
 package com.cashbook.cashbook.activity
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.os.Handler
 import android.os.Message
-import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import com.cashbook.cashbook.MainActivity
 import com.cashbook.cashbook.R
 
-class LauncherActivity : AppCompatActivity() {
+class LauncherActivity : Activity(), View.OnClickListener {
+    private val LAUNCHER_AD_TIME_COUNT_DOWN: Long = 3000
+    private val LAUNCH_DELAY = 1
+    private var mTimeCount = 3
 
     private lateinit var mHandler: Handler
     private lateinit var mThread: Thread
 
-    private val LAUNCH_DELAY = 1
+    private var mAvTimeTv: TextView? = null
+    private var mLaunchImage: ImageView? = null
+    private var mCountDownTimer: CountDownTimer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launcher)
 
+        initView()
+        initListener()
         jumpGuide()
+    }
+
+    private fun initListener() {
+        mAvTimeTv?.setOnClickListener { this }
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.launcher_av_time_tv) jumpMainActivity()
+    }
+
+    private fun initView() {
+        mAvTimeTv = findViewById(R.id.launcher_av_time_tv) as TextView?
+        mLaunchImage = findViewById(R.id.launcher_av_iv) as ImageView?
     }
 
     /**
@@ -35,22 +60,57 @@ class LauncherActivity : AppCompatActivity() {
                         intent.setClass(this@LauncherActivity, GuideActivity::class.java)
                         startActivity(intent)
                     } else {
-                        var intent = Intent()
-                        intent.setClass(this@LauncherActivity, MainActivity::class.java)
-                        startActivity(intent)
+                        startTimer(LAUNCHER_AD_TIME_COUNT_DOWN)
                     }
-                    //跳转后需要关闭页面
-                    finish()
+
                 }
             }
         }
         mThread = object : Thread() {
             override fun run() {
                 super.run()
-                mHandler.sendEmptyMessageDelayed(LAUNCH_DELAY, 2000)
+                var message = mHandler.obtainMessage(LAUNCH_DELAY)
+                mHandler.sendMessageDelayed(message, 1000)
             }
         }
         mThread.start()
+    }
+
+    private fun jumpMainActivity() {
+        var intent = Intent()
+        intent.setClass(this@LauncherActivity, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun startTimer(time: Long) {
+        //开始计时，则显示广告页和计时文字
+        mLaunchImage?.setImageResource(R.drawable.av)
+        mAvTimeTv?.visibility = View.VISIBLE
+        // 如果倒计时不为空，则先取消之前倒计时
+        cancelTimer()
+        // 启动倒计时
+        var timer = object : CountDownTimer(time, 1000) {
+            override fun onFinish() {
+                Log.i("Tag", mTimeCount.toString())
+                mAvTimeTv?.text = String.format("%s  秒跳转", mTimeCount)
+                jumpMainActivity()
+                //跳转后需要关闭页面
+                finish()
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                Log.i("Tag", mTimeCount.toString())
+                mAvTimeTv?.text = String.format("%s  秒跳转", mTimeCount)
+                mTimeCount -= 1
+            }
+        }
+        timer.start()
+    }
+
+    private fun cancelTimer() {
+        if (mCountDownTimer != null) {
+            mCountDownTimer = null
+        }
     }
 
     /**
@@ -73,6 +133,14 @@ class LauncherActivity : AppCompatActivity() {
         }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mHandler != null) {
+            mHandler.removeMessages(LAUNCH_DELAY)
+        }
+        cancelTimer()
+        System.gc()
+    }
 }
 
 
